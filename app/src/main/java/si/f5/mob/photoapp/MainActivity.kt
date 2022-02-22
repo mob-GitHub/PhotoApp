edab16f97240d2acf7608ab1aafcb2bcd2f8f154
mob-GitHub <halu.nomi@gmail.com>
2022-02-23 01:32:23 +0900

    MainActivityにMainScreenへの参照追加

--- app/src/main/java/si/f5/mob/photoapp/MainActivity.kt
+++ app/src/main/java/si/f5/mob/photoapp/MainActivity.kt
@@ -2,9 +2,14 @@ package si.f5.mob.photoapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            MainScreen()
+        }
     }
 }
