5a19be5db5ff994d9875b0f9ddfe3f610c4c1a89
mob <halu.nomiyama@gmail.com>
2022-07-24 13:24:05 +0900

    AccompanistPermissionを導入して権限取得処理を実装

--- app/src/main/java/si/f5/mob/photoapp/main/MainScreen.kt
+++ app/src/main/java/si/f5/mob/photoapp/main/MainScreen.kt
@@ -1,18 +1,49 @@
 package si.f5.mob.photoapp.main
 
+import android.Manifest
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
 import si.f5.mob.photoapp.component.PhotoGrid
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(
+    ExperimentalFoundationApi::class,
+    ExperimentalPermissionsApi::class
+)
 @Composable
 fun MainScreen(viewModel: MainViewModel, navController: NavController) {
     val imageList = viewModel.imageList.observeAsState()
 
-    viewModel.getImageList()
+    // 権限チェック
+    val permissionState =
+        rememberPermissionState(permission = Manifest.permission.READ_EXTERNAL_STORAGE)
+    when (permissionState.status) {
+        PermissionStatus.Granted -> {
+            viewModel.getImageList()
+        }
+        is PermissionStatus.Denied -> {
+            AlertDialog(
+                onDismissRequest = {},
+                title = { Text(text = "写真へのアクセス許可のお願い") },
+                text = { Text(text = "写真へのアクセスを許可してください") },
+                confirmButton = {
+                    TextButton(onClick = {
+                        permissionState.launchPermissionRequest()
+                    }) {
+                        Text(text = "OK")
+                    }
+                }
+            )
+        }
+    }
 
     Scaffold {
         imageList.value?.let {
