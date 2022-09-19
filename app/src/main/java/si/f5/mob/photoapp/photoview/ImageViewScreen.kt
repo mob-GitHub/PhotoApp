96f56d3b815b36420a827e06da8fd851133a89b3
mob <halu.nomiyama@gmail.com>
2022-09-19 10:59:35 +0900

    拡大・縮小機能の実装

--- app/src/main/java/si/f5/mob/photoapp/photoview/ImageViewScreen.kt
+++ app/src/main/java/si/f5/mob/photoapp/photoview/ImageViewScreen.kt
@@ -1,41 +1,84 @@
 package si.f5.mob.photoapp.photoview
 
 import android.net.Uri
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import kotlin.math.roundToInt
 
 @Composable
 fun ImageViewScreen(navController: NavController, imageUri: Uri?) {
     Scaffold { paddingValues ->
-        if (imageUri == null) {
-            AlertDialog(
-                onDismissRequest = {},
-                title = { Text(text = "エラー") },
-                text = { Text(text = "URLが空のため、表示できませんでした。") },
-                confirmButton = {
-                    TextButton(
-                        onClick = {
-                            navController.popBackStack()
-                        }) {
-                        Text(text = "OK")
-                    }
-                })
-        } else {
-            AsyncImage(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues.calculateBottomPadding()),
-                model = imageUri,
-                contentDescription = ""
-            )
+        BoxWithConstraints(modifier = Modifier.padding(paddingValues)) {
+            if (imageUri == null) {
+                EmptyUriErrorAlert(navController = navController)
+            } else {
+                ImageView(imageUri = imageUri)
+            }
         }
+    }
+}
+
+@Composable
+private fun EmptyUriErrorAlert(navController: NavController) {
+    AlertDialog(onDismissRequest = {},
+        title = { Text(text = "エラー") },
+        text = { Text(text = "URLが空のため、表示できませんでした。") },
+        confirmButton = {
+            TextButton(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "OK")
+            }
+        })
+}
+
+@Composable
+private fun ImageView(imageUri: Uri?) {
+    var offsetX by remember {
+        mutableStateOf(0f)
+    }
+    var offsetY by remember {
+        mutableStateOf(0f)
+    }
+    var scale by remember {
+        mutableStateOf(1f)
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+            detectTransformGestures { _, _, zoom, _ ->
+                scale = maxOf(1f, scale * zoom)
+            }
+        }
+    ) {
+        AsyncImage(modifier = Modifier
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+                    offsetX += dragAmount.x
+                    offsetY += dragAmount.y
+                }
+            }
+            .graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+            }
+            .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) },
+            model = imageUri,
+            contentDescription = ""
+        )
     }
 }
