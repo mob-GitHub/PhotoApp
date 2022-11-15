f37e1634a27de8759090ce5171e751841f10ba13
mob <halu.nomiyama@gmail.com>
2022-11-16 01:28:31 +0900

    ImageViewScreen->PhotoViewScreenに変更＆ViewModelの生成をhiltViewModel()に変更

---
+++ app/src/main/java/si/f5/mob/photoapp/photoview/PhotoViewScreen.kt
@@ -0,0 +1,50 @@
+package si.f5.mob.photoapp.photoview
+
+import android.graphics.Bitmap
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun PhotoViewScreen(
+    photoViewViewModel: PhotoViewViewModel = hiltViewModel(), imageId: Long?
+) {
+    val imageBitmap: Bitmap? by photoViewViewModel.imageBitmap.observeAsState()
+
+    photoViewViewModel.getImageBitmap(imageId = imageId)
+
+    Scaffold { paddingValues ->
+        BoxWithConstraints(modifier = Modifier.padding(paddingValues)) {
+            when (imageBitmap) {
+                null -> {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text(
+                            modifier = Modifier.align(Alignment.Center), text = "画像が取得できませんでした"
+                        )
+                    }
+                }
+                else -> {
+                    ImageView(bitmap = imageBitmap!!)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ImageView(bitmap: Bitmap) {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        drawImage(bitmap.asImageBitmap())
+    }
+}
---
+++ app/src/main/java/si/f5/mob/photoapp/photoview/PhotoViewViewModel.kt
@@ -0,0 +1,55 @@
+package si.f5.mob.photoapp.photoview
+
+import android.app.Application
+import android.graphics.Bitmap
+import androidx.core.graphics.drawable.toBitmapOrNull
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import coil.ImageLoader
+import coil.request.ErrorResult
+import coil.request.ImageRequest
+import coil.request.SuccessResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import si.f5.mob.mediastore.MediaStore
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class PhotoViewViewModel @Inject constructor(
+    private val mediaStore: MediaStore,
+    application: Application
+) : AndroidViewModel(application) {
+    private val _imageBitmap = MutableLiveData<Bitmap?>()
+    val imageBitmap: LiveData<Bitmap?>
+        get() = _imageBitmap
+
+    fun getImageBitmap(imageId: Long?) = viewModelScope.launch {
+        if (imageId == null) {
+            _imageBitmap.postValue(null)
+        }
+
+        val image = mediaStore.getImageUriById(imageId!!)
+        if (image == null) {
+            _imageBitmap.postValue(null)
+        }
+
+        Timber.d("Image(id = ${image!!.id}, uri = ${image.uri}, name = ${image.name}, width = ${image.width}, height = ${image.height})")
+
+        val request = ImageRequest.Builder(getApplication())
+            .data(image.uri)
+            .build()
+        val result = when (val result = ImageLoader(getApplication()).execute(request)) {
+            is SuccessResult -> {
+                result.drawable
+            }
+            is ErrorResult -> {
+                throw IllegalStateException("画像取得に失敗")
+            }
+        }
+
+        _imageBitmap.postValue(result.toBitmapOrNull())
+    }
+}
