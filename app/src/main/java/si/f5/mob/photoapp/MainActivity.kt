24d13d5f1aa562bfa14f28851a2236413a5bcedc
mob <halu.nomiyama@gmail.com>
2023-02-11 09:19:29 +0900

    ComposeをBoM方式に変更

--- app/src/main/java/si/f5/mob/photoapp/MainActivity.kt
+++ app/src/main/java/si/f5/mob/photoapp/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,6 +16,7 @@ import si.f5.mob.photoapp.main.MainScreen
 import si.f5.mob.photoapp.photoview.PhotoViewScreen
 
 @AndroidEntryPoint
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/si/f5/mob/photoapp/TopAppBar.kt
+++ app/src/main/java/si/f5/mob/photoapp/TopAppBar.kt
@@ -1,9 +1,11 @@
 package si.f5.mob.photoapp
 
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PhotoAppTopAppBar() {
     TopAppBar(
