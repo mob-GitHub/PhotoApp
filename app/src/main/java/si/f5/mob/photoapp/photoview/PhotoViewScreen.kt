7f8e19373ce0adfd1b83ddb7cacb6671c4a941b5
mob <halu.nomiyama@gmail.com>
2023-03-18 18:22:41 +0900

    編集用ImageViewを作成

--- app/src/main/java/si/f5/mob/photoapp/photoview/PhotoViewScreen.kt
+++ app/src/main/java/si/f5/mob/photoapp/photoview/PhotoViewScreen.kt
@@ -2,20 +2,23 @@ package si.f5.mob.photoapp.photoview
 
 import android.graphics.Bitmap
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -59,7 +62,7 @@ fun PhotoViewScreen(
                         }
                     }
                     else -> {
-                        ImageView(bitmap = imageBitmap!!)
+                        EditImageView(bitmap = imageBitmap!!)
                     }
                 }
             } else {
@@ -76,9 +79,37 @@ fun PhotoViewScreen(
     }
 }
 
+/**
+ * 編集用ImageView
+ * 自由に動かすことができる
+ */
 @Composable
-private fun ImageView(bitmap: Bitmap) {
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        drawImage(bitmap.asImageBitmap())
+private fun EditImageView(bitmap: Bitmap) {
+    val imageSize = IntSize(500, 500)
+    var offset by remember { mutableStateOf(IntOffset(50, 50)) }
+
+    Canvas(modifier = Modifier
+        .aspectRatio(1f)
+        .padding(all = 10.dp)
+        .border(width = 1.dp, color = Color.Gray)
+        .pointerInput(Unit) {
+            detectDragGestures { _, dragAmount ->
+                val nextOffset = offset + IntOffset(dragAmount.x.toInt(), dragAmount.y.toInt())
+                // 画像右端座標
+                val imageRightX = nextOffset.x + imageSize.width
+                // 画像下端座標
+                val imageBottomY = nextOffset.y + imageSize.height
+                if (nextOffset.x >= 0 && nextOffset.y >= 0 && imageRightX <= size.width && imageBottomY <= size.height) {
+                    offset = nextOffset
+                }
+            }
+        }) {
+        // 背景
+        drawRect(color = Color.White, size = size)
+        drawImage(
+            image = bitmap.asImageBitmap(),
+            srcSize = imageSize,
+            dstOffset = offset
+        )
     }
 }
